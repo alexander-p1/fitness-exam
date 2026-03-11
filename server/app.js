5256e01e1f7630e181f92f326f3f150d7118a993
alexander-p1 <alexanderpettersson_@hotmail.com>
2026-03-11 11:58:21 +0100

    Backend boilerplate, databas, express

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import { connectDB } from "./config/db.js";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+/* Framtida routes */
+// app.use("/api/auth", authRoutes);
+// app.use("/api/workouts", workoutRoutes);
+// app.use("/api/exercises", exercisesRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Appen är igång" });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
