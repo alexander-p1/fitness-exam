e8bc55bbbe48ebfd2b1985390880733843c7db99
alexander-p1 <alexanderpettersson_@hotmail.com>
2026-03-12 11:30:17 +0100

    Add user model with password hash and password comparison

---
+++ server/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Namn krävs"],
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: [true, "Email krävs"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: [true, "Lösenord krävs"],
+      minlength: [6, "Lösenordet måste vara minst 6 tecken"],
+    },
+  },
+  { timestamps: true },
+);
+
+// Checkar om lösen ändrats innan användaren sparas i databasen
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (inputPassword) {
+  return bcrypt.compare(inputPassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
