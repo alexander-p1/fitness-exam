0b64883acee54b7e260d303089a0e4c4a64c8469
alexander-p1 <alexanderpettersson_@hotmail.com>
2026-03-27 20:46:52 +0100

    merge

--- server/routes/exerciseRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getExercises } from "../controllers/exerciseController.js";
-import protectedRoute from "../middlewares/authMiddleware.js";
-
-const router = express.Router();
-
-router.get("/", protectedRoute, getExercises);
-
-export default router;
